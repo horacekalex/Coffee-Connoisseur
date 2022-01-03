8bc18ab6e64e336d29adae57af60b14f171bc786
Alexandra Horacek <>
2022-01-03 10:07:09 +0200

    added coffeestores on page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import styles from '../styles/Home.module.css'
 
 import Banner from '../components/banner';
 import Card from '../components/card';
+
+import coffeeStores from '../data/coffee-stores.json'
+
 export default function Home() {
   const handleOnBannerBtnClick = () => {
 
@@ -21,12 +24,16 @@ export default function Home() {
           <Image src='/static/hero-image.png' width={700} height={400}/>
         </div>
         <div className={styles.cardLayout}>
-          <Card
-          name='DarkHorse Coffee'
-          imgUrl='/static/hero-image.png'
-          href='/coffee-store/darkhorse-coffee'
-          className={styles.card}
-          />
+          {coffeeStores.map((coffeeStore) => {
+            return (
+                 <Card
+                  name={coffeeStore.name}
+                  imgUrl={coffeeStore.imgUrl}
+                  href={`/coffee-store/${coffeeStore.id}`}
+                  className={styles.card}
+                />
+            );
+          })}
         </div>
       </main>
 
