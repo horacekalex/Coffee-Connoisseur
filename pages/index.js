2c91883df3cf753cc86c252cf26a1db325c6a91a
Alexandra Horacek <>
2022-01-03 10:39:20 +0200

    prerending in app using getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,16 @@ import Card from '../components/card';
 
 import coffeeStores from '../data/coffee-stores.json'
 
-export default function Home() {
+export async function getStaticProps(context) {
+  return {
+    props: {
+      coffeeStores
+    }, // will be passed to the page component as props
+  }
+}
+
+export default function Home(props) {
+    console.log('props', props);
   const handleOnBannerBtnClick = () => {
 
   }
@@ -27,6 +36,7 @@ export default function Home() {
           {coffeeStores.map((coffeeStore) => {
             return (
                  <Card
+                  key={coffeeStore.id}
                   name={coffeeStore.name}
                   imgUrl={coffeeStore.imgUrl}
                   href={`/coffee-store/${coffeeStore.id}`}
