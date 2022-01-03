ed5d3d15739da2aeb59d360c8860945e17693461
Alexandra Horacek <>
2022-01-03 10:53:34 +0200

    added heading to coffeeStore page, changed naming of coffeeStoresData

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ import styles from '../styles/Home.module.css'
 import Banner from '../components/banner';
 import Card from '../components/card';
 
-import coffeeStores from '../data/coffee-stores.json'
+import coffeeStoresData from '../data/coffee-stores.json'
 
 export async function getStaticProps(context) {
   return {
     props: {
-      coffeeStores
+      coffeeStores: coffeeStoresData,
     }, // will be passed to the page component as props
   }
 }
@@ -32,19 +32,25 @@ export default function Home(props) {
         <div className={styles.heroImage}>
           <Image src='/static/hero-image.png' width={700} height={400}/>
         </div>
-        <div className={styles.cardLayout}>
-          {coffeeStores.map((coffeeStore) => {
-            return (
-                 <Card
-                  key={coffeeStore.id}
-                  name={coffeeStore.name}
-                  imgUrl={coffeeStore.imgUrl}
-                  href={`/coffee-store/${coffeeStore.id}`}
-                  className={styles.card}
-                />
-            );
-          })}
-        </div>
+        {props.coffeeStores.length > 0 && (
+        <>
+          <h2 className={styles.heading2}> Toronto Stores
+          </h2>
+          <div className={styles.cardLayout}>
+            {props.coffeeStores.map((coffeeStore) => {
+              return (
+                  <Card
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={coffeeStore.imgUrl}
+                    href={`/coffee-store/${coffeeStore.id}`}
+                    className={styles.card}
+                  />
+              );
+            })}
+          </div>
+        </>
+        )}
       </main>
 
     </div>
